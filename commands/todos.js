e1af6cde635a571a52e41fcfc374d59079f58bbe
Anton N <at0xa32@gmail.com>
2025-06-11 11:03:32 +0300

    add more information to tasks

--- commands/todos.js
+++ commands/todos.js
@@ -1,5 +1,11 @@
 import { getTodoList } from '../services/getTodoList.js';
 
+const priorityList = {
+  0: 'низкий',
+  1: 'средний',
+  2: 'высокий'
+}
+
 export default async function todosCommand(bot, msg, userTokens) {
   const chatId = msg.chat.id;
   const token = userTokens[chatId];
@@ -11,7 +17,13 @@ export default async function todosCommand(bot, msg, userTokens) {
 
   try {
     const todos = await getTodoList(token);
-    const text = todos.map(t => `• ${t.title}`).join('\n');
+
+    const text = todos.map(t => {
+      const title = `• ${t.title}\n`;
+      const priority = `• Приоритет: ${priorityList[t.priority]}\n`;
+      const description = `• Описание: ${t.description}\n`;
+      return `${title}${priority}${description}`
+    }).join('\n');
     bot.sendMessage(chatId, `Ваши задачи:\n${text}`);
   } catch (e) {
     bot.sendMessage(chatId, 'Ошибка при получении задач');
